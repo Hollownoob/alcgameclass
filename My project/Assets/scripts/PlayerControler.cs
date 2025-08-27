fa8af791c78306653b07d58dcdb025e4e491c603
Hollownoob <117678513+Hollownoob@users.noreply.github.com>
2025-08-27 14:12:45 -0600

    tank prototype

---
+++ My project/Assets/scripts/PlayerControler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float speed;
+    public float rotSpeed;
+    public float hInput;
+    public float vInput;
+    public float jumpForce;
+    public float firePoint;
+    public Rigidbody playerRB;
+
+    void update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        transform.Rotate(Vector3.up, hInput * rotSpeed * Time.deltaTime);
+        transform.Translate(Vector3.forward * vInput * speed * Time.deltaTime);
+    }
+}
