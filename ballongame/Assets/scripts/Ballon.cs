c71acd228667767ed42a60bac56f61af412ddd73
Hollownoob <117678513+Hollownoob@users.noreply.github.com>
2025-09-09 14:21:11 -0600

    updated ballon

---
+++ ballongame/Assets/scripts/Ballon.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Ballon : MonoBehaviour
+{
+    public int clickToPop = 3;
+    public float scaleToIncrease = 0.10f;
+    public int scoreToGive;
+    private ScoreManager scoreManager;
+    //public GameObject popEffect;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+    }
+
+    // Update is called once per frame
+    void onMouseDown()
+    {
+        clickToPop -=1;
+        transform.localScale += Vector3.one * scaleToIncrease;
+        if(clickToPop == 0)
+        {
+            scoreManager.IncreaseScoreText(scoreToGive);
+            //Instantiate(popEffect, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
