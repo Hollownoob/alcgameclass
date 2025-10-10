9914a5a398579c15db8c5bec5acc44a78cb55a01
Hollownoob <117678513+Hollownoob@users.noreply.github.com>
2025-10-10 14:15:47 -0600

    coding player and new scene

--- 2dplatformerselfmade/Assets/scripts/PlayerControler.cs
+++ 2dplatformerselfmade/Assets/scripts/PlayerControler.cs
@@ -1,25 +1,55 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PlayerControler : MonoBehaviour
 {
-    public int speed = 10;
-    public int jumpPower = 10;
-    public int xInput;
-    public int yInput;
+    [Header("Player settings")]
+    public float moveSpeed;
+    public float jumpForce;
+    public bool isGrounded;
+    public int bottomBound = -4;
+    public int score;
+    public Rigidbody2D rig;
+    public TextMeshProUGUI scoreText;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void addScore(int amount)
     {
-        
+        score += amount;
+        scoreText.text = "score: " + score;
     }
 
     // Update is called once per frame
+    void fixedUpdate()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rig.velocity = new Vector2(moveInput * moveSpeed, rig.velocity.y);
+    }
     void Update()
     {
-        transform.Translate(Vector2.right * xInput * speed * Time.deltaTime);
-        transform.Translate(Vector2.up * yInput * jumpPower * Time.deltaTime);
+        if(Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+        {
+            isGrounded = false;
+            rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        if(transform.position.y < bottomBound)
+        {
+            GameOver();
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.GetContact(0).normal == Vector2.up)
+        {
+            isGrounded = true;
+        }
+    }
+    public void GameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
